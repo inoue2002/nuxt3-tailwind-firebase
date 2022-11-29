ecd25d9ee9924d8e461a35a9e7a1f3f04bfc86e3
inoue2002 <54356188+youkan2002@users.noreply.github.com>
2022-11-29 15:56:38 +0900

    update module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,20 @@
-import { defineNuxtConfig } from 'nuxt/config'
-
-// https://v3.nuxtjs.org/guide/directory-structure/nuxt.config
 export default defineNuxtConfig({
-  publicRuntimeConfig: {
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
-    FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
-    FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
-    FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
-    FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+  runtimeConfig: {
+    public: {
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_AUTH_DOMAIN: process.env.FIREBASE_AUTH_DOMAIN,
+      FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
+      FIREBASE_STORAGE_BUCKET: process.env.FIREBASE_STORAGE_BUCKET,
+      FIREBASE_MESSAGING_SENDER_ID: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      FIREBASE_APP_ID: process.env.FIREBASE_APP_ID,
+    },
   },
   nitro: {
-    preset: "vercel",
+    preset: 'vercel',
   },
-  css: ["@/assets/css/tailwind.css"],
+  css: ['@/assets/css/tailwind.css'],
   // 参考:https://std9.jp/articles/01fq14pxsp4r6nbyzp2txj46ac
-  build: {
-    postcss: {
-      postcssOptions: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-    },
+  postcss: {
+    plugins: { tailwindcss: {} },
   },
 });
